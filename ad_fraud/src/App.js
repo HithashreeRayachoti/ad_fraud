ddf39452d15ffb3b134e3deffca60b14cd8a1e39
HithashreeRayachoti <hithashreerayachoti@gmail.com>
2025-07-17 15:27:24 +0530

    Changes in frontend with added logo

--- ad_fraud/src/App.js
+++ ad_fraud/src/App.js
@@ -1,17 +1,14 @@
 import React, { useEffect } from "react";
 import './App.css';
-import { logVisit, startTracking, endTracking } from "./clickceaseTracker";
+import { startTracking, endTracking } from "./clickceaseTracker";
 
 function App() {
   useEffect(() => {
-    // Start tracking on mount
     console.log("Starting tracking...");
     startTracking();
 
-    // Send logs when tab is closed/refreshed
     window.addEventListener("beforeunload", endTracking);
 
-    // Cleanup
     return () => {
       window.removeEventListener("beforeunload", endTracking);
     };
@@ -22,23 +19,31 @@ function App() {
       <h1 className="heading">Welcome to Our Ad Landing Page</h1>
       <p className="subheading">This simulates what happens when someone clicks on your ad.</p>
 
+      {/* ✅ Centered image below heading */}
+     <img
+  src="/kpmg-logo-1.png"
+  alt="KPMG Logo"
+  className="center-logo"
+/>
+
+
       <div className="link-grid">
-        <a className= 'link' href="https://www.example.com/product1" target="_blank" rel="noopener noreferrer">
+        <a className='link' href="https://www.example.com/product1" target="_blank" rel="noopener noreferrer">
           Product 1
         </a>
-        <a className= 'link' href="https://www.example.com/product2" target="_blank" rel="noopener noreferrer" >
+        <a className='link' href="https://www.example.com/product2" target="_blank" rel="noopener noreferrer">
           Product 2
         </a>
-        <a className= 'link' href="https://www.example.com/about" target="_blank" rel="noopener noreferrer" >
+        <a className='link' href="https://www.example.com/about" target="_blank" rel="noopener noreferrer">
           About Us
         </a>
-        <a className= 'link' href="https://www.example.com/services" target="_blank" rel="noopener noreferrer" >
+        <a className='link' href="https://www.example.com/services" target="_blank" rel="noopener noreferrer">
           Services
         </a>
-        <a className= 'link' href="https://www.example.com/contact" target="_blank" rel="noopener noreferrer" >
+        <a className='link' href="https://www.example.com/contact" target="_blank" rel="noopener noreferrer">
           Contact Us
         </a>
-        <a className= 'link' href="https://www.example.com/blog" target="_blank" rel="noopener noreferrer">
+        <a className='link' href="https://www.example.com/blog" target="_blank" rel="noopener noreferrer">
           Blog
         </a>
       </div>
